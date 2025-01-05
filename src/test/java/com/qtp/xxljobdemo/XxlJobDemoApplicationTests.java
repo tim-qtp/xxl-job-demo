0d74fe77840bfb689a0ef4ea17cfdb6b1f836090
tim-qtp <2469100031@qq.com>
2025-01-05 20:19:16 +0800

    初始化项目

---
+++ src/test/java/com/qtp/xxljobdemo/XxlJobDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qtp.xxljobdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxlJobDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
